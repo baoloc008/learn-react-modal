7eddced04aaf5b4b9d3a05962657ce543764b4a9
baoloc008 <baoloc008@gmail.com>
2021-04-10 17:59:39 +0700

    add modal style

--- src/base-modal/BaseModal.js
+++ src/base-modal/BaseModal.js
@@ -5,10 +5,22 @@ import {
   enableBodyScroll,
   clearAllBodyScrollLocks,
 } from "body-scroll-lock";
+import styles from "./styles.module.css";
 
 ReactModal.setAppElement("#root");
 
-function Modal({ isOpen, closeModal, children }) {
+function Modal({
+  isOpen = false,
+  closeModal,
+  children,
+  modalContentClassName = styles.modalContent,
+  modalContentAfterOpenClassName = "",
+  modalContentBeforeCloseClassName = "",
+  modalOverlayClassName = styles.modalOverlay,
+  modalOverlayAfterOpenClassName = "",
+  modalOverlayBeforeCloseClassName = "",
+  ...otherProps
+}) {
   const modalRef = useRef(null);
 
   useEffect(() => {
@@ -23,7 +35,22 @@ function Modal({ isOpen, closeModal, children }) {
   }, [isOpen]);
 
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={closeModal} ref={modalRef}>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      ref={modalRef}
+      className={{
+        base: modalContentClassName,
+        afterOpen: modalContentAfterOpenClassName,
+        beforeClose: modalContentBeforeCloseClassName,
+      }}
+      overlayClassName={{
+        base: modalOverlayClassName,
+        afterOpen: modalOverlayAfterOpenClassName,
+        beforeClose: modalOverlayBeforeCloseClassName,
+      }}
+      {...otherProps}
+    >
       {children}
     </ReactModal>
   );
