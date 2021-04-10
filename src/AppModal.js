086401ca29c00269f65f4f36bae2c35208bee69c
baoloc008 <baoloc008@gmail.com>
2021-04-10 17:34:12 +0700

    disable body scroll

--- src/AppModal.js
+++ src/AppModal.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BaseModal from "./base-modal";
+import LongContent from "./LongContent";
 
 function AppModal({ isOpen, onCloseModal }) {
   return (
     <BaseModal isOpen={isOpen} closeModal={onCloseModal}>
       <p>This is modal</p>
       <button onClick={onCloseModal}>CloseModal</button>
+      <LongContent />
     </BaseModal>
   );
 }
--- src/base-modal/BaseModal.js
+++ src/base-modal/BaseModal.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactModal from "react-modal";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 ReactModal.setAppElement("#root");
 
 function Modal({ isOpen, closeModal, children }) {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      disableBodyScroll(modalRef.current);
+    } else {
+      enableBodyScroll(modalRef.current);
+    }
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, [isOpen]);
+
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={closeModal}>
+    <ReactModal isOpen={isOpen} onRequestClose={closeModal} ref={modalRef}>
       {children}
     </ReactModal>
   );
